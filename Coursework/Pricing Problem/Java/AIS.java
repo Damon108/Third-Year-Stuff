43cfa4a41264900e988736ce40ba2218c262a0a2
Damon Tran <trand3@aston.ac.uk>
2018-12-08 23:59:51 +0000

    Coursework stuff /08/12/18

--- Coursework/Pricing Problem/Java/AIS.java
+++ Coursework/Pricing Problem/Java/AIS.java
@@ -13,13 +13,13 @@ public class AIS {
 	private int u;
 	private Double temp;
 	private double revenue;
-	private HashMap<ArrayList<double[]>, Double> hmap;
+	private HashMap<double[], Double> hmap;
 	private ArrayList<double[]> solutions;
 	private double normalisedFitness;
 
 	public AIS() {
 		temp = new Double(revenue);
-		hmap = new HashMap<ArrayList<double[]>, Double>();
+		hmap = new HashMap<double[], Double>();
 		solutions = new ArrayList<double[]>();
 		u = 5;
 		rng = new Random();
@@ -33,11 +33,11 @@ public class AIS {
 
 		generateRandomPrices();
 
-		for (Map.Entry<ArrayList<double[]>, Double> me : hmap.entrySet()) {
+		for (Map.Entry<double[], Double> me : hmap.entrySet()) {
 
 			System.out.println("Key is at the end" + me.getKey() + " " + "value end: " + me.getValue());
 		}
-
+		contigMutate();
 		// getBestFitness();
 		// createInitialPop();
 
@@ -54,12 +54,12 @@ public class AIS {
 				System.out.println(randomPrices[i]);
 			}
 
-			// hmap = new HashMap<ArrayList<double[]>, Double>();
+			// hmap = new HashMap<double[], Double>();
 			revenue = f.evaluate(randomPrices);
 
 			temp = new Double(revenue);
 			solutions.add(randomPrices);
-			hmap.put(solutions, temp);
+			hmap.put(randomPrices, temp);
 
 			// System.out.println("revenue is " + hmap.get(solutions));
 
@@ -76,10 +76,10 @@ public class AIS {
 			tempS = generateRandomPrices();
 			revenue = f.evaluate(randomPrices);
 			temp = new Double(revenue);
-			hmap.put((ArrayList<double[]>) tempS.clone(), temp);
+			// hmap.put((ArrayList<double[]>) tempS.clone(), temp);
 		}
 
-		for (ArrayList<double[]> key : hmap.keySet()) {
+		for (double[] key : hmap.keySet()) {
 
 			System.out.println("key " + key);
 			System.out.println("value : " + hmap.get(key));
@@ -101,7 +101,7 @@ public class AIS {
 	private double[] NormaliseFitness() {
 		double[] nF = new double[u];
 		int i = 0;
-		for (Map.Entry<ArrayList<double[]>, Double> me : hmap.entrySet()) {
+		for (Map.Entry<double[], Double> me : hmap.entrySet()) {
 			nF[i] = me.getValue() / getBestFitness();
 			// System.out.println("Key is " + me.getKey() + " " + "value : " +
 			// me.getValue());
@@ -111,10 +111,10 @@ public class AIS {
 		return nF;
 	}
 
-	public HashMap<ArrayList<double[]>, Double> clone() {
-		HashMap<ArrayList<double[]>, Double> clone1 = new HashMap<ArrayList<double[]>, Double>();
+	public HashMap<double[], Double> clone() {
+		HashMap<double[], Double> clone1 = new HashMap<double[], Double>();
 		clone1 = hmap;
-		for (Map.Entry<ArrayList<double[]>, Double> me : clone1.entrySet()) {
+		for (Map.Entry<double[], Double> me : clone1.entrySet()) {
 
 			System.out.println("clone Key is at the end" + me.getKey() + " " + "clone value end: " + me.getValue());
 		}
@@ -122,41 +122,48 @@ public class AIS {
 		return clone1;
 	}
 
-	private void contigMutate(int p, int l) {
-		ArrayList<HashMap<ArrayList<double[]>, Double>> clonalPool = new ArrayList<HashMap<ArrayList<double[]>, Double>>();
+	private void contigMutate() {
+		ArrayList<HashMap<double[], Double>> clonalPool = new ArrayList<HashMap<double[], Double>>();
 		clonalPool = createClonalPool();
-		double[] tempPrices = new double[numberofGoods];
-		p = 4;
-		l = -p * NormaliseFitness();
-		double mutationRate = 0;
-		// double mutationRate;
-		int hotspot;
 		Random rnd = new Random();
-		for (HashMap<ArrayList<double[]>, Double> solutions : clonalPool) {
-			int counter =0;
-			for (ArrayList<double[]> priceSolutions : solutions.keySet()) {
-				mutationRate = -p * NormaliseFitness()[counter];
-				
-				for (double[] prices : priceSolutions) {
+		int p = 4;
+		// l = -p * NormaliseFitness();
+		double mutationRate = 0;
+		HashMap<double[], Double> bigBoi = new HashMap<double[], Double>();
+
+		for (HashMap<double[], Double> solutions : clonalPool) {
+			int counter = 0;
+			// for (ArrayList<double[]> priceSolutions : solutions.keySet()) {
+
+			for (double[] prices : solutions.keySet()) {
+				mutationRate = 1 * NormaliseFitness()[counter];
+				System.out.println("mutation rate is " + mutationRate);
 				double length = numberofGoods * mutationRate;
-					
-					for (int i = p; i < length; i++) {
-					
-						prices[i] = (-prices[i] * NormaliseFitness()[counter]);
-					}
-					tempPrices = prices;
+				System.out.println("lengh is " + length);
+				
+				for (int i = p; i < length; i++) {
+
+					prices[i] = (prices[i] * mutationRate);
+					System.out.println("new prices " + prices[i]);
 				}
+
+				double revenue = f.evaluate(prices);
+				Double temp = new Double(revenue);
+				bigBoi.put(prices, temp);
+				// tempPrices = prices;
 				counter++;
 			}
-			
+
 		}
+
+		// }
 		// p = rnd.nextInt(numberofGoods);
 		// l = numberofGoods * mutationRate;
 	}
 
-	public ArrayList<HashMap<ArrayList<double[]>, Double>> createClonalPool() {
+	public ArrayList<HashMap<double[], Double>> createClonalPool() {
 		int numOfClones = 5;
-		ArrayList<HashMap<ArrayList<double[]>, Double>> clones = new ArrayList<HashMap<ArrayList<double[]>, Double>>();
+		ArrayList<HashMap<double[], Double>> clones = new ArrayList<HashMap<double[], Double>>();
 		for (int i = 0; i < numOfClones; i++) {
 			clones.add(clone());
 		}
